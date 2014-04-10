099c1122b3725752504f073e1154523d0e8c8c1c
Alexandre Beaudet <selena.zelia@gmail.com>
2014-04-10 14:44:52 +0200

    Creation of the repository

---
+++ VirtualWar/src/com/bht/Game.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+package com.bht;
+
+/**
+ * @author Alexandre Beaudet
+ * 
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
